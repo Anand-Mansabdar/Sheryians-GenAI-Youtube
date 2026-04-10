071141ee23990845fd546db25112339a5c986d1d
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-04-11 00:11:30 +0530

    Integrating Roles to the Chatbot

--- ChatBot_Project/chatbot.py
+++ ChatBot_Project/chatbot.py
@@ -1,23 +1,25 @@
 from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 
 load_dotenv()
 
 model = ChatMistralAI(model="mistral-small-2506", temperature=0.9)
 
-history = []
+history = [
+  SystemMessage(content="You are a funny AI agent")
+]
 
 print("Welcome to the AI chatbot... Type Ctrl+C to exit")
 while True:
   
   prompt = input("Your message: ")
-  history.append(prompt)
+  history.append(HumanMessage(content=prompt))
   
-  if prompt == 0:
+  if prompt == "0":
     break
   
   response = model.invoke(history)
-  history.append(response.content)
+  history.append(AIMessage(content=response.content))
   print("Bot: ",response.content)
-  
-  
+print(history)
